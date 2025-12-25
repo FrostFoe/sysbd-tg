110c266df7b201b24edd974434491eeb9c2bb3ab
FrostFoe <131578101+FrostFoe7@users.noreply.github.com>
2025-12-25 16:34:24 +0000

    safe

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,8 +1,8 @@
 import { Client, Account, Databases, Storage } from "appwrite";
 
 const client = new Client()
-  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT)
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "https://cloud.appwrite.io/v1")
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || "dummy-project");
 
 const account = new Account(client);
 const databases = new Databases(client);
